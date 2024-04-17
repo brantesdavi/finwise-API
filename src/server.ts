886c5d20dfca5cd86421379c68fd19778f7875f2
davib <davi.brantes@gmail.com>
2024-04-16 22:09:52 -0300

    refactor: refatorado rota de criação para possibilitar a criação com recorrencia, quantidade e endDate sendo nulos ou undefined

--- src/server.ts
+++ src/server.ts
@@ -41,49 +41,51 @@ app.get('/transactions', async (req, res) => {
 
 //Create transaction
 app.post('/transactions', async (req, res) => {
-    try {
-        const createTransactionSchema = z.object({
-            spendOrIncome: z.boolean(),
-            title: z.string(),
-            price: z.number(),
-            date: z.string().pipe(z.coerce.date()),
-            categoryId: z.number(),
-            userId: z.string(),
-            recurrrenceUnit: z.string().nullable(),
-            quantity: z.number().nullable(),
-            endDate: z.string().pipe(z.coerce.date()).nullable()
-        })
+  try {
+    const createTransactionSchema = z.object({
+      spendOrIncome: z.boolean(),
+      title: z.string(),
+      price: z.number(),
+      date: z.string().pipe(z.coerce.date()),
+      categoryId: z.number(),
+      userId: z.string(),
+      recurrrenceUnit: z.string().nullable().transform((val) => (val === '' ? null : val)).optional(),
+      quantity: z.number().optional(),
+      endDate: z.string().nullable().transform((val) => (val === '' ? null : val)).optional(),
+  });
+  
 
-        const { 
-            spendOrIncome, 
-            title, 
-            price, 
-            date, 
-            categoryId, 
-            userId, 
-            recurrrenceUnit, 
-            quantity, 
-            endDate 
-        } = createTransactionSchema.parse(req.body)
+      const { 
+          spendOrIncome, 
+          title, 
+          price, 
+          date, 
+          categoryId, 
+          userId, 
+          recurrrenceUnit, 
+          quantity, 
+          endDate 
+      } = createTransactionSchema.parse(req.body)
 
-        await prisma.transaction.create({
-            data: {
-                spendOrIncome,
-                title,
-                price,
-                date,
-                categoryId,
-                userId,
-                recurrrenceUnit,
-                quantity,
-                endDate
-            }
-        })
-        res.status(201).send();
-    } catch (error) {
-        res.status(400).send(error);
-    }
+      await prisma.transaction.create({
+          data: {
+              spendOrIncome,
+              title,
+              price,
+              date,
+              categoryId,
+              userId,
+              recurrrenceUnit,
+              quantity,
+              endDate,
+          }
+      })
+      res.status(201).send();
+  } catch (error) {
+      res.status(400).send(error);
+  }
 });
+
 
 app.listen({
     host: '0.0.0.0',
